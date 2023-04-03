11948a8baf7b131405eb8705f2992bf484fa5062
sabi3097 <sabi30970@gmail.com>
2023-04-03 03:52:39 -0500

    updates, main for webserver and requeriments

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,15 @@
 import store
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,4]
+
+@app.get('/contact')
+def get_contact():
+    return {'name' : 'PYTHON'}
 
 def run():
     store.get_categories()
